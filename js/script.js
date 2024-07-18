e8ca36e88f4aedb330b9fff4e2acb3880f76cba3
abdul-muhaimin-toha <abdulmuhaimintoha.business@gmail.com>
2024-07-18 13:17:13 +0600

    add:load more btn functionality done

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const mobileNavOpenerBtn = document.querySelector('#mobile-nav-opener-btn');
 const mobileNavCloseBtn = document.querySelector('#mobile-nav-close-btn');
 const mainNav = document.querySelector('.navigation');
 const mobileNav = document.querySelector('.mobile-navigation');
+const loadMoreBtn = document.querySelector('#load-more');
+const blogContainer = document.querySelector('.blog__container');
 
 // /////////////////////////////////
 // Navigation Functionality
@@ -50,4 +52,34 @@ $(document).ready(function () {
       return '<div class="custom-pagination-2"></div>';
     },
   });
+});
+
+loadMoreBtn.addEventListener('click', function () {
+  const newBlogCard = `
+    <div class="blog-carousal blog-simple">
+      <div class="blog-carousal__slider">
+        <img src="img/blogs/blog-3.jpg" alt="Blog image" />
+      </div>
+      <div class="blog-carousal__content blog-video__content">
+        <div class="blog-carousal__details">
+          <p class="blog-carousal__category">Design & Creative</p>
+          <p class="blog-carousal__date">10 June 2023</p>
+        </div>
+        <h2 class="blog-carousal__heading">
+          Lorem ipsum dolor sit amet consectetur
+        </h2>
+        <p class="blog-carousal__description">
+          Lorem ipsum dolor sit amet consectetur. Et bibendum nunc in
+          amet auctor enim massa. Leo purus amet sem libero quisque.
+        </p>
+        <p>By <span class="blog-carousal__author">Jon Doe</span></p>
+      </div>
+    </div>
+  `;
+
+  // Append the new blog card to the container
+  for (let i = 0; i < 3; i++) {
+    blogContainer.insertAdjacentHTML('beforeend', newBlogCard);
+    loadMoreBtn.style.display = 'none';
+  }
 });
